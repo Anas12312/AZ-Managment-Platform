ba8150fbc978449a480819dd2863c9afba095fb3
Zyad Abdul-Nasser <zizo_007@live.com>
2023-04-28 03:21:31 +0300

    'zyad'

--- src/app.js
+++ src/app.js
@@ -3,11 +3,14 @@ require('./db/mongoose')
 const userRouter = require('./routers/user')
 const unitRouter = require('./routers/unit')
 const nodeRouter = require('./routers/node')
+const accountRouter = require('./routers/account')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+
+app.use(accountRouter)
 app.use(userRouter)
 // app.use(unitRouter)
 // app.use(nodeRouter)
--- src/routers/user.js
+++ src/routers/user.js
@@ -55,35 +55,4 @@ router.delete('/users/:id', async (req, res) => {
     }
 })
 
-//LOGIN
-router.post('/users/login', async (req, res) => {
-    try {
-        const user = await User.findByCredentials(req.body.email, req.body.password)
-        const token = await user.generateAuthToken()
-        res.send({ user, token })
-    }catch(error) {
-        res.status(404).send({error: error.message})
-    }
-})
-
-router.post('/users/logout', auth, async (req, res) => {
-    try {
-        req.user.tokens = req.user.tokens.filter((token) => token.token !== req.token)
-        await req.user.save()
-        res.send()
-    } catch (error) {
-        res.status(500).send()
-    }
-})
-
-router.post('/users/logout-all', auth, async (req, res) => {
-    try {
-        req.user.tokens = []
-        await req.user.save()
-        res.send()
-    } catch (error) {
-        res.status(500).send()
-    }
-})
-
 module.exports = router
