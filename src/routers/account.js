09caab99425eb7a765a18c6a3e570e239960f625
anas12312 <anashesham000@gmail.com>
2023-05-22 01:42:00 +0300

    'anas'

--- src/routers/account.js
+++ src/routers/account.js
@@ -5,7 +5,6 @@ const auth = require('../middleware/auth')
 
 //LOGIN
 router.post('/account/login', async (req, res) => {
-    console.log(req.body)
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
         const token = await user.generateAuthToken()
