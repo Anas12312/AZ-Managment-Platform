46c80bd7ba71aaa575ec694e3cbefcae7424c4a2
Zyad Abdul-Nasser <zizo_007@live.com>
2023-07-02 20:45:25 +0300

    'zyad'

--- src/models/resource.js
+++ src/models/resource.js
@@ -32,6 +32,12 @@ const schema = new mongoose.Schema({
     createdBy: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
+    },
+    createdByName: {
+        type: String
+    },
+    createdByImageUrl: {
+        type: String
     }
 }, 
 {
